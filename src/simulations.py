8c54d27347b785ede0306ae9d32ebe0c33449589
Lucas Diogo Martins <lucas.d.martins@unesp.br>
2026-01-30 08:29:11 -0300

    "numpy save/load

--- src/simulations.py
+++ src/simulations.py
@@ -2,7 +2,7 @@ from src.config import np, njit
 
 
 @njit
-def throughput(arrival_prob, send_prob, time=10000):
+def throughput(arrival_prob, send_prob, time):
     """
     Throughput total do sistema (soma de todos os nós)
     """
@@ -42,7 +42,7 @@ def throughput(arrival_prob, send_prob, time=10000):
 
 
 @njit
-def occupation_up_pack(arrival_prob, send_prob, time=100):
+def occupation_up_pack(arrival_prob, send_prob, time):
     """L_up -> probabilidade de encontrar um pacote que será transmitido com
     sucesso, olhando para algum timeslot aleatório"""
 
@@ -83,7 +83,7 @@ def occupation_up_pack(arrival_prob, send_prob, time=100):
     return total_occupation/time
 
 
-def mean_delay_up_pack(arrival_prob, send_prob, time=100):
+def mean_delay_up_pack(arrival_prob, send_prob, time):
     """
     tempo médio de permanência dos pacotes de uptade no nó (W_up)
     """
@@ -120,3 +120,64 @@ def mean_delay_up_pack(arrival_prob, send_prob, time=100):
             B_have = True
 
     return total_delay / number_successes if number_successes > 0 else None
+
+
+# @njit
+# def AAoI_PAoI_sim(arrival_prob, send_prob, time=100):
+
+#     # rolagem de todos os dados
+#     arrivals_A = np.random.random(time) < arrival_prob
+#     arrivals_B = np.random.random(time) < arrival_prob
+#     sends_A = np.random.random(time) < send_prob
+#     sends_B = np.random.random(time) < send_prob
+
+#     # Do ponto de vista do destino, a idade inicial é 0 (ou o tempo atual)
+#     current_aoi_A = 0
+#     successes = 0
+#     AAoI = 0
+#     PAoI = 0
+
+#     # Variáveis dos transmissores
+#     tA_arrival = 0
+#     A_have = False
+#     B_have = False
+
+#     for t in range(time):
+
+#         # 1. A idade no destino sempre aumenta em 1 a cada time step
+#         current_aoi_A += 1
+
+#         # 2. Lógica de Transmissão (acontece no slot)
+#         A_tries = A_have and sends_A[t]
+#         B_tries = B_have and sends_B[t]
+
+#         # Sucesso de A
+#         if A_tries and not B_tries:
+#             PAoI += current_aoi_A - 1
+#             current_aoi_A = (t - tA_arrival) + 1
+#             successes += 1
+#             A_have = False
+
+#         # Sucesso de B
+#         if B_tries and not A_tries:
+#             B_have = False
+
+#         # 3. Lógica de Chegada (com substituição). Desde o tempo 0, pode ter pacotes.
+#         # aqui, ficou mais fácil deixar as chegadas neste ponto
+#         if arrivals_A[t]:
+#             tA_arrival = t
+#             A_have = True
+
+#         if arrivals_B[t]:
+#             B_have = True
+
+#     # 4. Armazena o estado do AoI no final do processo
+#     AAoI += current_aoi_A
+
+#     if successes > 0:
+#         AAoI = AAoI / time
+#         PAoI = PAoI / successes
+#     else:
+#         AAoI = None
+#         PAoI = None
+#     return (AAoI, PAoI)
